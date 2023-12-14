ed0143df130248a1057096958d5e07c581119191
Mantas Skara <mantas.skara@gmail.com>
2023-12-14 19:17:48 +0200

    add countries file

---
+++ countries.py
@@ -0,0 +1,15 @@
+class Country:
+    def __init__(self, name, population, area):
+        self.name = name
+        self.population = population
+        self.area = area
+        self.is_big = self.check_if_big()
+
+    def check_if_big(self):
+        return self.population > 20000000 or self.area > 3000000
+
+australia = Country("Australia", 23545500, 7692024)
+print(australia.is_big)
+
+lithuania = Country("Lithuania",2700000,635000)
+print(lithuania.is_big)
