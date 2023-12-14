c6381122247ce2d9312cbdced87cdc75d6823e9d
MSkara <mantas.skara@gmail.com>
2023-12-14 19:19:28 +0200

    Update to 1500000

--- countries.py
+++ countries.py
@@ -6,7 +6,7 @@ class Country:
         self.is_big = self.check_if_big()
 
     def check_if_big(self):
-        return self.population > 20000000 or self.area > 3000000
+        return self.population > 15000000 or self.area > 3000000
 
 australia = Country("Australia", 23545500, 7692024)
 print(australia.is_big)
